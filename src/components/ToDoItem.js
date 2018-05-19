cebe315a58ed36680597d9a0f80bce561ca81e25
Justin Gatlin <vikingstamp@live.com>
2018-05-19 16:37:50 -0400

    fixed comma error

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const ToDoItem = ({props}) => {
+const ToDoItem = ({ props }) => {
   console.log("ToDoItem", props);
   return (
     <div className="item">
       <div>{this.props.item.name}</div>
-      <div>{this,props.item.description}</div>
+      <div>{this.props.item.description}</div>
       <div>{this.props.item.duedate}</div>
       <div>{this.props.item.status}</div>
     </div>
